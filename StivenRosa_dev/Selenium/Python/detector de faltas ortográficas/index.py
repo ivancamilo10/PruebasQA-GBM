27a7cb6b7b9f18881a4f84f39fcf3fdc17621680
Stiven <codestiven@gmail.com>
2025-09-20 11:44:22 -0400

    implementacion de nuevo verificador de fartas

--- StivenRosa_dev/Selenium/Python/detector de faltas ortográficas/index.py
+++ StivenRosa_dev/Selenium/Python/detector de faltas ortográficas/index.py
@@ -6,7 +6,8 @@ import time
 
 
 from ElementData import ElementData
-from detector import detectar_falta_ortografica
+# from detector import detectar_falta_ortografica
+from revisar_ortografia import revisar_ortografia as detectar_falta_ortografica
 
 def initialize_driver(URL='https://www.google.com'):
     driver = webdriver.Chrome()
@@ -50,17 +51,20 @@ def extract_elements_info(driver):
 def main():
     try:
         elementos_faltas = []
+        elementos_bien = []
         driver = initialize_driver('https://tecnomarketrd.com/')
         time.sleep(1)
         elementos_info = extract_elements_info(driver)
 
         for elemento in elementos_info:
-            if detectar_falta_ortografica(elemento.text)[0] == "1":
+            # print(detectar_falta_ortografica(elemento.text))
+            if detectar_falta_ortografica(elemento.text)[0] == 1:
                 elementos_faltas.append(elemento)
+            else:
+                elementos_bien.append(elemento)
 
-        print(f"Elementos con faltas ortográficas: {elementos_faltas[0].text}")
-
-
+        print(f"Elementos con faltas ortográficas: {len(elementos_faltas)}")
+        print(f"Elementos sin faltas ortográficas: {len(elementos_bien)}")
 
 
 
