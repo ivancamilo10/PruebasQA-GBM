7dc1ab7a85db5b346a060717b2ab69c95ab35271
Stiven <codestiven@gmail.com>
2025-09-20 10:26:07 -0400

    Agregar clases y funciones para la detección de faltas ortográficas en elementos HTML

---
+++ StivenRosa_dev/Selenium/Python/detector de faltas ortográficas/ElementData.py
@@ -0,0 +1,9 @@
+class ElementData:
+    def __init__(self, tag, text, element_id, element_class, location, displayed, is_active):
+        self.tag = tag
+        self.text = text
+        self.element_id = element_id
+        self.element_class = element_class
+        self.location = location
+        self.displayed = displayed
+        self.is_active = is_active
---
+++ StivenRosa_dev/Selenium/Python/detector de faltas ortográficas/index.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException, TimeoutException
+import time
+
+
+from ElementData import ElementData
+from detector import detectar_falta_ortografica
+
+def initialize_driver(URL='https://www.google.com'):
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    return driver
+
+
+
+
+
+
+def extract_elements_info(driver):
+    etiquetas = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 
+                 'li', 'span', 'a', 'strong', 'em', 
+                 'blockquote', 'label', 'button', 'td', 'th']
+    
+    elementos_info = []
+
+    for tag in etiquetas:
+        elementos = driver.find_elements(By.TAG_NAME, tag)
+        for el in elementos:
+            if el.text.strip():
+                data = ElementData(
+                    tag=el.tag_name,
+                    text=el.text.strip(),
+                    element_id=el.get_attribute("id"),
+                    element_class=el.get_attribute("class"),
+                    location=el.location,
+                    displayed=el.is_displayed(),
+                    is_active=el.is_enabled()
+                )
+                elementos_info.append(data)
+
+    return elementos_info
+
+
+
+
+
+
+def main():
+    try:
+        elementos_faltas = []
+        driver = initialize_driver('https://tecnomarketrd.com/')
+        time.sleep(1)
+        elementos_info = extract_elements_info(driver)
+
+        for elemento in elementos_info:
+            if detectar_falta_ortografica(elemento.text)[0] == "1":
+                elementos_faltas.append(elemento)
+
+        print(f"Elementos con faltas ortográficas: {elementos_faltas[0].text}")
+
+
+
+
+
+
+
+
+
+
+
+
+    except (WebDriverException, TimeoutException) as e:
+        print(f"❌ Ocurrió un error al abrir la página o con el driver: {e}")
+    finally:
+        try:
+            driver.quit()
+        except:
+            pass
+
+
+
+
+if __name__ == '__main__':
+    main()
