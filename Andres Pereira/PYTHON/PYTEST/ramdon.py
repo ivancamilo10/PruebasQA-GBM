1b2f2d7ed6d0eb1584c0e7bc5bb34720e40998b4
Wendy esquea <wendyesquea984@gmail.com>
2025-09-29 12:01:14 -0500

    carpeta de wendy

--- Andres Pereira/PYTHON/PYTEST/ramdon.py
+++
@@ -1,9 +0,0 @@
-import random
-
-estudiantes = ["Ana", "Luis", "Carlos", "Marta", "Sofía", "Jorge", "Lucía",
-               "Diego", "Elena", "Pablo", "Carmen", "Alberto", "Isabel", "Fernando", "Raquel"]
-
-
-estudiante_seleccionado = random.randint(0, len(estudiantes)-1)
-
-print(f"El estudiante ganador es :{estudiantes[estudiante_seleccionado]}")
